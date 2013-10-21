a4134fc09f99ba5c164e4f18cdb5f68ca2d4c2cf
Jeffrey Chen <jeffreyhc@berkeley.edu>
2013-10-21 02:32:57 -0700

    Added a file to handle Users
    Currently still building it right now.

---
+++ app/models/user.rb
@@ -0,0 +1,6 @@
+class User < ActiveRecord::Base 
+	validates :first_name, presence: true, length {minimum: 1}
+	validates :last_name, presence: true, length {minimum: 1}
+	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+	validates :email, presence: true, format: { with: VALID_EMAIL_REGEX}, uniqueness: {case_sensitive: false}
+end
