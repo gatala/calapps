427b5af5108b46236d38d8da373498c8082f6cd8
Jeffrey Chen <jeffreyhc@berkeley.edu>
2013-10-23 16:00:18 -0700

    Added another migration for Users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20131018054550) do
+ActiveRecord::Schema.define(:version => 20131023224520) do
 
   create_table "calapps", :force => true do |t|
     t.string   "name"
@@ -20,6 +20,13 @@ ActiveRecord::Schema.define(:version => 20131018054550) do
     t.text     "description"
     t.datetime "created_at",  :null => false
     t.datetime "updated_at",  :null => false
+  end
+
+  create_table "users", :force => true do |t|
+    t.string   "name"
+    t.string   "email"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
   end
 
 end
