f1cad51d2525bd416a327a063a25219ae175d8b9
Jeffrey Chen <jeffreyhc@berkeley.edu>
2013-10-23 15:48:33 -0700

    Generated the controller, view, and database for users
    All rails autogenerated.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,3 @@
-class User < ActiveRecord::Base 
-	validates :first_name, presence: true, length {minimum: 1}
-	validates :last_name, presence: true, length {minimum: 1}
-	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-	validates :email, presence: true, format: { with: VALID_EMAIL_REGEX}, uniqueness: {case_sensitive: false}
+class User < ActiveRecord::Base
+  attr_accessible :email, :name
 end
