ccd6d5d380a1953308ff0b32171648fdc319b76f
Jeffrey Chen <jeffreyhc@berkeley.edu>
2013-10-25 04:44:56 -0700

    Changed the root route.

--- config/routes.rb
+++ config/routes.rb
@@ -13,7 +13,7 @@ Calapps::Application.routes.draw do
   # Sample of named route:
   #   match 'products/:id/purchase' => 'catalog#purchase', :as => :purchase
   # This route can be invoked with purchase_url(:id => product.id)
-
+  match '/signup', to: 'users#new'
   # Sample resource route (maps HTTP verbs to controller actions automatically):
   #   resources :products
   resources :calapps
