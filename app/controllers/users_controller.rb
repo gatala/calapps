37c1338319ea17a9d030d75ecd1631da174e6984
Jeffrey Chen <jeffreyhc@berkeley.edu>
2013-10-30 17:39:54 -0700

    Changed up UsersController
    Finished the function "new", "show", and "index". Still cannot figure
    out the signup form.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,17 +1,25 @@
 class UsersController < ApplicationController
-  def new
-  	@user = User.new
-  	if @user.save 
-  	else 
-  		render 'new'
+	def new
+  		@user = User.new
+		if @user.save 
+  			flash[:success] = "Welcome to Cal Apps!"
+  			redirect_to @user
+  		else 
+  			render 'new'
+  		end
   	end
-  end
 
-  def show 
-  	@user = User.find(params[:id])
-  end 
+	def show 
+  		@user = User.find(params[:id])
+  	end 
 
-  def index 
-  	@users = User.all
-  end
+	def index 
+  		@users = User.all
+  	end
+
+	private
+	def user_params
+    	params.require(:user).permit(:name, :email, :password,
+            :password_confirmation)
+    end
 end
