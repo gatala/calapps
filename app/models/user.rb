2da6ede72890aa567772b01420a94ef8bed04677
Jeffrey Chen <jeffreyhc@berkeley.edu>
2013-11-02 15:26:41 -0700

    Added Cookies

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ActiveRecord::Base
 
   before_save {self.email = email.downcase}
+  before_create :create_remember_token
+
   validates :name, :presence => true, length: {:within => 3..25}
 
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
@@ -10,4 +12,18 @@ class User < ActiveRecord::Base
 
   has_secure_password
   validates :password, length: {:within => 8..20}
+
+  def User.new_remember_token
+    SecureRandom.urlsafe_base64
+  end
+
+  def User.encrypt(token)
+    Digest::SHA1.hexdigest(token.to_s)
+  end
+
+  private
+
+    def create_remember_token
+      self.remember_token = User.encrypt(User.new_remember_token)
+    end
 end
