2021df20d5ae4648a6b699e7c54f05900092aade
Jeffrey Chen <jeffreyhc@berkeley.edu>
2013-11-01 18:10:17 -0700

    Working on cookies

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,7 +5,7 @@ class SessionsController < ApplicationController
 	end 
 
 	def create 
-		user = User.find_by(email: params[:session][:email].downcase)
+		user = User.find(email: params[:session][:email].downcase).first
   		if user && user.authenticate(params[:session][:password])
     		sign_in user
       		redirect_to user
