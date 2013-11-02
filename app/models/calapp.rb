ff6a6db9a3eaaf67a1261a9e6e28b7c361118ed4
amd851 <amd851@gmail.com>
2013-11-01 17:12:46 -0700

    more validation for calapp model, inc valid URL check

--- app/models/calapp.rb
+++ app/models/calapp.rb
@@ -1,5 +1,14 @@
 class Calapp < ActiveRecord::Base
 	belongs_to :user
+
 	validates_uniqueness_of :name
-	validates_uniqueness_of :URL
+
+	validates :name, :presence => true, length: {:within => 3..25}
+
+	VALID_URL_REGEX = /^[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?$/ix
+
+	validates :URL, :presence => true, 
+  					format: { with: VALID_URL_REGEX },
+  					uniqueness: { case_sensitive: false }
+
 end
