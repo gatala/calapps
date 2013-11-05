cd05bc970ac9c14c28ce558ff51f99d4eca38ca2
amd851 <amd851@gmail.com>
2013-11-05 12:26:36 -0800

    minor update to calapp_contoller_spec
    renamed calapps_controller_spec to calapp_controller_spec

---
+++ spec/contollers/calapp_controller_spec.rb
@@ -0,0 +1,29 @@
+require 'spec_helper'
+
+describe CalappsController do
+	describe 'Editing Calapps' do
+
+		it 'create movie' do
+			calapp = double('Calapp')
+			calapp.stub(:name)
+			calapp.stub(:creator)
+			calapp.stub(:URL)
+
+			Calapp.should_receive(:create!).and_return(calapp)
+			post :create, {:calapp => calapp}
+			response.should redirect_to(calapps_path)		
+		end
+
+		it 'delete movie' do
+	    	calapp = double('Calapp')
+			calapp.stub(:name)
+			calapp.stub(:creator)
+			calapp.stub(:URL)
+	      
+	    	Calapp.should_receive(:find).with('9').and_return(calapp)
+	    	calapp.should_receive(:destroy)
+	    	post :destroy, {:id => '9'}
+	    	response.should redirect_to(calapps_path)			
+		end
+	end
+end
