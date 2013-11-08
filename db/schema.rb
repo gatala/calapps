9fb02dea0450e7ae6a71acc3667620a12bcf5283
Jeffrey Chen <jeffreyhc@berkeley.edu>
2013-11-08 15:40:32 -0800

    Officially migrate created_location

--- db/schema.rb
+++ db/schema.rb
@@ -11,15 +11,16 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20131108225305) do
+ActiveRecord::Schema.define(:version => 20131108233903) do
 
   create_table "calapps", :force => true do |t|
     t.string   "name"
     t.string   "URL"
     t.string   "creator"
     t.text     "description"
-    t.datetime "created_at",  :null => false
-    t.datetime "updated_at",  :null => false
+    t.datetime "created_at",       :null => false
+    t.datetime "updated_at",       :null => false
+    t.string   "created_location"
   end
 
   create_table "taggings", :force => true do |t|
