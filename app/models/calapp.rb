ff83464d3776e0c044e9dee6c766e9cb73d20433
Jeffrey Chen <jeffreyhc@berkeley.edu>
2013-11-08 15:04:29 -0800

    Tags are up
    fixing the bug of too much protection LOL.

--- app/models/calapp.rb
+++ app/models/calapp.rb
@@ -1,6 +1,10 @@
 class Calapp < ActiveRecord::Base
 	#belongs_to :user
 
+	attr_accessible :name, :creator, :tag_list
+	
+	acts_as_taggable 
+
 	validates :name, :presence => true, length: {:within => 3..25}, 
   			uniqueness: { case_sensitive: false }
 
--- db/schema.rb
+++ db/schema.rb
@@ -11,16 +11,32 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20131108084206) do
+ActiveRecord::Schema.define(:version => 20131108225305) do
 
   create_table "calapps", :force => true do |t|
     t.string   "name"
     t.string   "URL"
     t.string   "creator"
     t.text     "description"
-    t.datetime "created_at",       :null => false
-    t.datetime "updated_at",       :null => false
-    t.string   "created_location"
+    t.datetime "created_at",  :null => false
+    t.datetime "updated_at",  :null => false
+  end
+
+  create_table "taggings", :force => true do |t|
+    t.integer  "tag_id"
+    t.integer  "taggable_id"
+    t.string   "taggable_type"
+    t.integer  "tagger_id"
+    t.string   "tagger_type"
+    t.string   "context",       :limit => 128
+    t.datetime "created_at"
+  end
+
+  add_index "taggings", ["tag_id"], :name => "index_taggings_on_tag_id"
+  add_index "taggings", ["taggable_id", "taggable_type", "context"], :name => "index_taggings_on_taggable_id_and_taggable_type_and_context"
+
+  create_table "tags", :force => true do |t|
+    t.string "name"
   end
 
   create_table "users", :force => true do |t|
