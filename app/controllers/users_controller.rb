fe16e33b6e16233ec9d68a71fb3010f1e4835fa0
Benjamin Liu <bliu94539@gmail.com>
2013-11-07 23:05:13 -0800

    whee

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,54 +1,54 @@
 class UsersController < ApplicationController
-	#before_action :signed_in_user, only: [:edit, :update]
-	#before_action :correct_user, only: [:edit, :update]
-	def new
-    	@user = User.new
-  	end
+  #before_action :signed_in_user, only: [:edit, :update]
+  #before_action :correct_user, only: [:edit, :update]
+  def new
+    @user = User.new
+  end
 
-	def create
-  		@user = User.new(user_params)
-		if @user.save 
-  			flash[:success] = "Welcome to Cal Apps!"
-  			redirect_to @user
-  		else 
-  			render 'new'
-  		end
-  	end
-
-	def show 
-  		@user = User.find(params[:id])
-  	end 
-
-	def index 
-  		@users = User.all
-  	end
-
-  	def update
-  		 @user = User.find(params[:id])
-    	if @user.update_attributes(user_params)
-      		flash[:success] = "Profile udpated!"
-      		redirect_back_or @user
-    	else
-    		flash.new[:error] = 'Invalid email/password'
-      		render 'edit'
-    	end
-  	end
-
-  	def destroy 
-  		User.find(params[:id]).destroy
-    	flash[:success] = "User deleted."
-    	redirect_to users_url
+  def create
+    @user = User.new(user_params)
+    if @user.save 
+      flash[:success] = "Welcome to Cal Apps!"
+      redirect_to @user
+    else 
+      render 'new'
     end
+  end
 
-    def edit 
-    	@user = User.find(params[:id])
+  def show 
+    @user = User.find(params[:id])
+  end 
+
+  def index 
+    @users = User.all
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+      flash[:success] = "Profile udpated!"
+      redirect_back_or @user
+    else
+      flash.new[:error] = 'Invalid email/password'
+      render 'edit'
     end
+  end
 
-	private
-		def user_params
-    		params.require(:user).permit(:name, :email, :password,
-            	:password_confirmation)
-    	end
+  def destroy 
+    User.find(params[:id]).destroy
+    flash[:success] = "User deleted."
+    redirect_to users_url
+  end
+
+  def edit 
+    @user = User.find(params[:id])
+  end
+
+  private
+    def user_params
+      params.require(:user).permit(:name, :email, :password,
+            :password_confirmation)
+    end
 
 
     def signed_in_user
