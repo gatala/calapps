a3feb6a1e3f3797da9d621df8f331ac2361d5de5
amd851 <amd851@gmail.com>
2013-11-11 04:38:38 -0800

    admin make others admins, but needs pw. need to fix

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -69,7 +69,7 @@ class UsersController < ApplicationController
 	private
 		def user_params
     		params.require(:user).permit(:name, :email, :password,
-            	:password_confirmation)
+            	:password_confirmation, :is_admin)
     	end
 
 
