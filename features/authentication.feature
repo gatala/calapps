c2b9a44489b07b0a073dd9df4d5dbf60bac51d1e
amd851 <amd851@gmail.com>
2013-11-13 23:22:56 -0800

    merge fix

--- features/authentication.feature
+++ features/authentication.feature
@@ -5,12 +5,40 @@ User Story:
 	So that I can manage my apps
 	I want to be able to signin and create/edit/delete my own apps and no non-admin user should be able to do this
 
-User Story
+User Story:
 	As an admin
 	So that I can moderate apps
 	I want to be able to signin and create/edit/delete any app
 
 Scenario: Viewing List of Calapps as a visitor(non-signed-in user)
-	When
+	When I am not signed in and I am on the Calapps page
+	Then I should not see "New"
+
+Scenario: Visiting List of Calapps as a signed in user
+	When I am signed in as a user and I am on the Calapps page
+	Then I should see "New"
+
+Scenario: Visiting List of Calapps as an admin
+	When I am signed in as an admin and I am on the Calapps page
+	Then I should see "New"
+
+Scenario: Looking at a Calapp as a visitor (non-signed in user)
+	When I am not signed in and I am viewing an app
+	Then I should not see "Edit" or "Delete"
+
+Scenario: Looking at a Calapp as a non-creator user
+	When I am signed in and I am viewing an app I didn't upload
+	Then I should not see "Edit" or "Delete"
+
+Scenario: Looking at a Calapp as a creator
+	When I am signed in and I am viewing an app I uploaded
+	Then I should see "Edit" and "Delete"
+
+Scenario: Looking at a Calapp as an admin
+	When I am signed in as an admin and I am viewing an app
+	Then I should see "Edit" and "Delete"
+
+
+
 
 
--- features/step_definitions/authentication_steps.rb
+++ features/step_definitions/authentication_steps.rb
@@ -1,3 +1,6 @@
+require 'uri'
+require 'cgi'
+#require 'capybara/cucumber'
 Given /^a user visits the signin page$/ do
   visit signin_path
 end
@@ -28,3 +31,44 @@ end
 Then /^she should see a signout link$/ do
   expect(page).to have_link('Sign out', href: signout_path)
 end
+
+When(/^I am not signed in and I am on the Calapps page$/) do
+  #pending # express the regexp above with the code you wish you had
+  visit '/calapps'
+end
+
+# Then(/^I should not see "(.*?)"$/) do |arg1|
+#   pending # express the regexp above with the code you wish you had
+# end
+
+# When(/^I am signed in as a user and I am on the Calapps page$/) do
+#   pending # express the regexp above with the code you wish you had
+# end
+
+# When(/^I am signed in as an admin and I am on the Calapps page$/) do
+#   pending # express the regexp above with the code you wish you had
+# end
+
+# When(/^I am not signed in and I am viewing an app$/) do
+#   pending # express the regexp above with the code you wish you had
+# end
+
+# Then(/^I should not see "(.*?)" or "(.*?)"$/) do |arg1, arg2|
+#   pending # express the regexp above with the code you wish you had
+# end
+
+# When(/^I am signed in and I am viewing an app I didn't upload$/) do
+#   pending # express the regexp above with the code you wish you had
+# end
+
+# When(/^I am signed in and I am viewing an app I uploaded$/) do
+#   pending # express the regexp above with the code you wish you had
+# end
+
+# Then(/^I should see "(.*?)" and "(.*?)"$/) do |arg1, arg2|
+#   pending # express the regexp above with the code you wish you had
+# end
+
+# When(/^I am signed in as an admin and I am viewing an app$/) do
+#   pending # express the regexp above with the code you wish you had
+# end
