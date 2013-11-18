27fbbae5dcfbc686d07b5ed984614370631257b7
dvtran93 <saasbook@saasbook.(none)>
2013-11-17 23:25:30 -0800

    fixed password reset without matching email bug

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -1,8 +1,15 @@
 class PasswordResetsController < ApplicationController
 	def create
 		user = User.find_by_email(params[:email])
-		user.send_password_reset if user
-		redirect_to root_url, :notice => "Email sent with password reset instructions."
+		if (user != nil)
+			user.send_password_reset if user
+			redirect_to root_url, :notice => "Email sent with password reset instructions."
+		else
+			flash[:error] = 'error'
+			render 'new'
+		end
+
+		
 	end
 
 	def edit
