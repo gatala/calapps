ef093a8f7382ed81bce24999db91bc87efe50726
Jeffrey Chen <jeffreyhc@berkeley.edu>
2013-11-22 15:55:56 -0800

    Increasing Security
    Making sure that people also put a valid name and legitimate password
    (one capital letter and one number)

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,15 +5,23 @@ class User < ActiveRecord::Base
 
   before_save {self.email = email.downcase}
 
-  validates :name, :presence => true, length: {:within => 3..25}
+  #VALID_NAME_REGEX = /[a-zA-z]*\s[a-zA-Z]*\W[a-zA-z]*/
+  validates :name, 
+            :presence => true, 
+            length: {:within => 3..25},
+            #format: {with: VALID_NAME_REGEX}
 
+  
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, :presence => true, 
   					format: { with: VALID_EMAIL_REGEX },
   					uniqueness: { case_sensitive: false }
 
+  #VALID_PASSWORD_REGEX = /[a-zA-Z\d]*/
   has_secure_password
-  validates :password, length: {:within => 8..20, allow_nil: true}
+  validates :password, 
+            length: {:within => 8..20, allow_nil: true},
+            #format: {with: VALID_PASSWORD_REGEX}
   
   mount_uploader :image, ImageUploader
 
