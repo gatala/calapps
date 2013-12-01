1621332ba8b69860865794eb81b957c9b08badff
Jeffrey Chen <jeffreyhc@berkeley.edu>
2013-12-01 00:39:20 -0800

    Added default thumbnail
    Need to find a way to make sure that we save it in our own assets

--- app/helpers/calapps_helper.rb
+++ app/helpers/calapps_helper.rb
@@ -1,6 +1,11 @@
 module CalappsHelper
-
-
+	def thumbnail_url(calapp)
+		if calapp.image.present?
+			calapp.image
+		else
+			calapp.image = 'http://www.advancingjustice-alc.org/sites/asian-law-caucus/files/styles/publication-landing/public/default_images/aj-aai-publication-default-thumbnail_0.gif?itok=8wySC9YR'
+		end
+	end
 
 
 end
