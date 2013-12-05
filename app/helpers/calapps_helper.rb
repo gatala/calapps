7160d686a54a2fd517a497c63272616070383fa3
Jeffrey Chen <jeffreyhc@berkeley.edu>
2013-12-04 21:26:39 -0800

    Done.
    Need to tweak the UI and the defaults.

--- app/helpers/calapps_helper.rb
+++ app/helpers/calapps_helper.rb
@@ -7,5 +7,43 @@ module CalappsHelper
 		end
 	end
 
+	def screenshot1_url(calapp)
+		if calapp.screenshot1.present?
+			calapp.screenshot1
+		else
+			calapp.screenshot1 = ''
+		end
+	end
 
+	def screenshot2_url(calapp)
+		if calapp.screenshot2.present?
+			calapp.screenshot2
+		else
+			calapp.screenshot2 = ''
+		end
+	end
+
+	def screenshot3_url(calapp)
+		if calapp.screenshot3.present?
+			calapp.screenshot3
+		else
+			calapp.screenshot3 = ''
+		end
+	end
+
+	def screenshot4_url(calapp)
+		if calapp.screenshot4.present?
+			calapp.screenshot4
+		else
+			calapp.screenshot4 = ''
+		end
+	end
+
+	def screenshot5_url(calapp)
+		if calapp.screenshot5.present?
+			calapp.screenshot5
+		else
+			calapp.screenshot5 = ''
+		end
+	end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20131202112059) do
+ActiveRecord::Schema.define(:version => 20131205050900) do
 
   create_table "calapps", :force => true do |t|
     t.string   "name"
@@ -24,6 +24,17 @@ ActiveRecord::Schema.define(:version => 20131202112059) do
     t.string   "user_email"
     t.string   "category"
     t.string   "image"
+    t.string   "screenshot"
+    t.string   "screenshot1"
+    t.string   "screenshot2"
+    t.string   "screenshot3"
+    t.string   "screenshot4"
+    t.string   "screenshot5"
+  end
+
+  create_table "pictures", :force => true do |t|
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
     t.string   "screenshot"
   end
 
