8e31f997d087410d52320d6dc3d5b034a7055889
Jeffrey Chen <jeffreyhc@berkeley.edu>
2014-01-01 22:36:48 -0800

    Added a destroy method to User

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -53,6 +53,11 @@ class UsersController < ApplicationController
     end
   end
 
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+  end
+
   private
     def user_params
       params.require(:user).permit(:name, :email, :password,
